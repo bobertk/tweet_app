ba803f1f91e838f40b0a3f577cb11db7284f2191
Robert Karlsson <BobKarlsson@gmail.com>
2012-06-18 13:06:19 -0600

    finish layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 TweetApp::Application.routes.draw do
+  get "users/new"
+
 root to: 'static_pages#home' # when routed with "root to:" just remember to delete public/index.html.
 
+  match '/signup',  to: 'users#new'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact' 
